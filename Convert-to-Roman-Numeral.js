1e8c2021326f3a62a6d156d346039652b7b0360a
christien07 <85590547+christien07@users.noreply.github.com>
2021-12-14 20:12:23 -0700

    Add files via upload

---
+++ Convert-to-Roman-Numeral.js
@@ -0,0 +1,50 @@
+function convertToRoman(num) {
+    let romanNum = "";
+    
+    while(num > 0) {
+      if(num < 4) {
+        romanNum += "I";
+        num -= 1;
+      } else if(num === 4) {
+        romanNum += "IV";
+        num -= 4;
+      } else if(num >= 5 && num < 9) {
+        romanNum += "V";
+        num -= 5;
+      } else if(num === 9) {
+        romanNum += "IX";
+        num -= 9;
+      } else if(num >= 10 && num < 40) {
+        romanNum += "X";
+        num -= 10;
+      } else if(num >= 40 && num < 60) {
+        romanNum += "XL";
+        num -= 40;
+      } else if(num >= 50 && num < 90) {
+        romanNum += "L";
+        num -= 50;
+      } else if(num >= 90 && num < 100) {
+        romanNum += "XC";
+        num -= 90;
+      } else if(num >= 100 && num < 400) {
+        romanNum += "C";
+        num -= 100;
+      } else if(num >= 400 && num < 500) {
+        romanNum += "CD";
+        num -= 400;
+      } else if(num >= 500 && num < 900) {
+        romanNum += "D";
+        num -= 500;
+      } else if(num >= 900 && num < 1000) {
+        romanNum += "CM";
+        num -= 900;
+      } else {
+        //Any value entered >= 4000 would not return any special characters, only the standard I through M is used.
+        romanNum += "M";
+        num -= 1000;
+      }
+  
+    }
+    //Ensure that no matter what we recieve a Roman Numeral in Upper Case
+    return romanNum.toUpperCase(500);
+  }
